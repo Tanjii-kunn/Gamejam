8d5f0d41a56e848cd6e4212cdb1fa023e42ec74f
Tanjii-kunn <ujjwalkumar2681@gmail.com>
2025-03-16 13:54:22 +0530

    health

--- scripts/healthbarr.gd
+++ scripts/healthbarr.gd
@@ -3,8 +3,15 @@ extends TextureProgressBar
 @onready var timer: Timer = $Timer
 
 func _physics_process(delta: float) -> void:
-	if value == 0:
-		timer.start()
+	if value == 10:
+		print("jee")
+
+
+
+	if Input.is_action_just_pressed("mouse"):
+		value -= 1
+
 
 func _on_timer_timeout() -> void:
-	print("jello")
+	print("nigga")
+	
