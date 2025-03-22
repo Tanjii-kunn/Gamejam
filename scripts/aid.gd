b1d9d46e0a05081d287c414af45c9aaf516519e6
Tanjii-kunn <ujjwalkumar2681@gmail.com>
2025-03-22 10:18:42 +0530

    bet

--- scripts/aid.gd
+++ scripts/aid.gd
@@ -6,5 +6,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is player:
 		if body.cchealth < 10:
-			body.cchealth += randf_range(3, 6)
+			body.cchealth = 10
 			queue_free()
--- scripts/enebulletet.gd
+++ scripts/enebulletet.gd
@@ -12,3 +12,7 @@ func _on_body_entered(body):
 		body.cchealth -= body.dmg
 		self.queue_free()
 		
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
